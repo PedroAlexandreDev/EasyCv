b2f29ce6293405fa5da8dd27daa9766d7348f33b
Pedro Augsuto <pedroaugsutolinux@gmail.com>
2025-07-20 11:30:35 -0300

    create modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TemplatesModule } from './templates/templates.module';
+import { CreditsModule } from './credits/credits.module';
+import { ResumesModule } from './resumes/resumes.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, TemplatesModule, CreditsModule, ResumesModule],
   controllers: [AppController],
   providers: [AppService],
 })
